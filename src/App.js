6c8e1c008555852a644b3167c2d2bfb6a8cae0d7
smoralber <smoralber@gmail.com>
2021-12-30 14:47:14 -0500

    Added section item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import SectionHeader from './components/section-header/sectionHeader';
+import SectionItem from './components/section-item/sectionItem';
 
 // works as a lamba function (inside parenthesis you can pass props)
 const App = () =>
     <body>
       <div className="App"/>
       <SectionHeader/>
+      <SectionItem title = "Test Title" college = "Test college"
+      date = "Test date" description = "Capacidad de utilizar un enfoque sistemático, disciplinado y cuantificable para el desarrollo, operación y mantenimiento del software, de establecer y utilizar principios sólidos de ingeniería para obtener software fiable que funcione
+      eficientemente en máquinas reales así como de estimar los costes de los desarrollos.<br/><br/>Todo ello supone un conocimiento profundo de la programación de ordenadores, la implantación y planificación de sistemas informáticos
+      así como de las fases del ciclo de vida del software, desde una perspectiva práctica y adaptada a las realidades sociales de nuestro país." />
     </body>
 
 export default App;
--- src/components/section-item/sectionItem.js
+++ src/components/section-item/sectionItem.js
@@ -1,2 +1,16 @@
 import React from "react";
+import './sectionItem.css'
 
+const SectionItem = (props) => 
+<div className="section-container">
+    <div className="section-info">
+        <p className="item-title">{props.title}</p>
+        <p className="item-college">{props.college}</p>
+        <p className="item-date">{props.date}</p>
+    </div>
+    <div className="section-description">
+        <p>{props.description}</p>
+    </div>
+</div>
+
+export default SectionItem;
