10c90086f7ccf84baa2314a94ff1317c676d15aa
smoralber <smoralber@gmail.com>
2021-12-30 17:07:39 -0500

    Added translations

---
+++ public/strings/strings.js
@@ -0,0 +1,24 @@
+const strings = {
+    STUDIES_BACHELOR_DEGREE_TITLE = "Grado Ingeniería de software",
+    STUDIES_BACHELOR_DEGRE_COLLEGE = "Universidad Politécnica de Madrid",
+    STUDIES_BACHELOR_DEGRE_DATE = "2011- 2017",
+    STUDIES_BACHELOR_DEGRE_DESCRIPTION = "Capacidad de utilizar un enfoque sistemático, disciplinado y cuantificable para el desarrollo, operación y mantenimiento del software, de establecer y utilizar principios sólidos de ingeniería para obtener software fiable que funcione" +
+    " eficientemente en máquinas reales, así como de estimar los costes de los desarrollos.<br/><br/>Todo ello supone un conocimiento profundo de la programación de ordenadores, la implantación y planificación de sistemas informáticos" +
+    " así como de las fases del ciclo de vida del software, desde una perspectiva práctica y adaptada a las realidades sociales de nuestro país.",
+    
+    STUDIES_COURSE_ANDROID_TITLE = "Desarrollo Aplicaciones Android",
+    STUDIES_COURSE_ANDROID_COLLEGE = "Universidad Politécnica de Madrid - Samsung Tech Institute",
+    STUDIES_COURSE_ANDROID_DATE = "200 horas - 2014",
+    STUDIES_COURSE_ANDROID_DESCRIPTION = "Curso enfocado para estudiantes interesados en el desarrollo de aplicaciones para dispositivos Android. En este curso se abordan diferentes temas relacionados con el desarrollo de un aplicaión como:<br/><br/>• Identificación de idea" +
+    "para el proyecto<br/>• Creación de un mock up<br/>• Análisis de UX/UI<br/>• Desarrollo (JAVA)<br/>• Publicación de la aplicación en Play store",
+    
+    STUDIES_COURSE_WEB_TITLE = "Desarrollo Front-End con HTML5 y CSS3",
+    STUDIES_COURSE_WEB_COLLEGE = "Universidad Politécnica de Madrid - Samsung Tech Institute",
+    STUDIES_COURSE_WEB_DATE = "80 horas - 2016",
+    STUDIES_COURSE_WEB_DESCRIPTION = "Curso dirigido a desarrolladores/as y diseñadores/as web tanto nuevos como experimentados y ampliar sus habilidades para incluir los próximos estándares web. A través de laboratorios prácticos y ejercicios," +
+    "se aprendieron los fundamentos de la creación de páginas web con las normas/estándares actuales. El curso se centra en el lenguaje de maquetación de contenidos HTML5 y en las hojas de estilo en cascada proporcionadas por CSS3. Adicionalmente se introducen conceptos" +
+    "básicos de JavaScript y el uso de la biblioteca Bootstrap. Los/as alumnos/as adquirirán, entre otras, destrezas que le permitirán:<br/><br/>• Entender la arquitectura cliente-servidor" +
+    "y las características de los servidores web, considerando las peculiaridades de los clientes asociados." +
+    "<br/>• Conocer el lenguaje HTML para el desarrollo de páginas Web (en todas sus versiones), incluyendo textos, imágenes y otros contenidos multimedia.<br/>• Dotar a los documentos HTML de las capacidades proporcionadas por el CSS," +
+    " adaptándose a criterios de usabilidad y accesibilidad. <br/>• Entender cómo integrar el uso del lenguaje JavaScript"
+}
