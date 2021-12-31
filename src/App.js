6be73d13d9d4a5ec010df4651f0406edcde8feb7
smoralber <smoralber@gmail.com>
2021-12-30 19:02:02 -0500

    Auto detect when browser has different language

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import SectionHeader from './components/section-header/sectionHeader';
 import SectionItem from './components/section-item/sectionItem';
-import { useTranslation } from 'react-i18next'; 
+import { useTranslation } from 'react-i18next';
 
 export default function App() {
   const { t, i18n } = useTranslation();
@@ -12,6 +12,10 @@ export default function App() {
       <SectionHeader/>
       <SectionItem title = {t("studies_degree_title")} college = {t("studies_degree_college")} 
       date = {t("studies_degree_date")} description = {t("studies_degree_description")} />
+      <SectionItem title = {t("studies_course_android_title")} college = {t("studies_course_android_college")} 
+      date = {t("studies_course_android_date")} description = {t("studies_course_android_description")} />
+      <SectionItem title = {t("studies_course_web_title")} college = {t("studies_course_web_college")} 
+      date = {t("studies_course_web_date")} description = {t("studies_course_web_description")} />
     </body>
     )
 }
