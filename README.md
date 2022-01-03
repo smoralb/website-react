054da24e146cad580c98c8de404521d3af7674a8
smoralber <smoralber@gmail.com>
2022-01-03 14:06:41 -0500

    Add README info

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
+Frameworks: 
+    - React JS
+    - i18N
+    - Babel
+
+i18N
+
+This library helps with the internationalization of the website. It will change the language depending on the browser language and will get the strings from local files.
+
+For easier management of the translations I have installed the library i18next-http-backend. With this, we just need to follow this steps:
+
+    1. Install the library.
+
+        `npm install i18next-http-backend`
+
+    2. Import to the cofing file of i18N.
+
+        ```
+        import i18next from 'i18next';
+        import Backend from 'i18next-http-backend';
+        
+        i18next.use(Backend).init(i18nextOptions);
+        ```
+
+    3. Create translation files.
+        ```
+        public
+            /_locales
+                /_en
+                    /_ translation.json
+                /_es
+                    /_ translation.json
+                ...
+        ```
+
 Nice to read
 
 i18n -> https://codetain.com/blog/how-to-translate-react-application-with-react-i18next-part-2
+
---
+++ src/components/section-contactme/section-contact.js
@@ -0,0 +1,24 @@
+<div class="container-fluid contact-me-container">
+<div style="padding: 0 20px;" class="row">
+    <div class="col-sm-4 center-absolute">
+        <img class="mx-auto d-block" id="portrait-img" src="images/portrait.jpeg" alt="portrailt"/>
+    </div>
+    <div class="col-sm-8 center-absolute" style="margin: 0px;">
+        <h1 id="contact-title">Contact me!</h1>
+        <div class="row" style="padding-top: 50px;">
+            <div class="column col-sm-4 center-absolute" style="text-align: center;">
+                <i class="fa fa-map-marker center-absolute" style="font-size:48px;color:white;padding-bottom: 20px;"></i>
+                <p>Madrid, Spain</p>
+            </div>
+            <div class="column col-sm-4 center-absolute" style="text-align: center;">
+                <i class="fa fa-envelope" style="font-size:48px;color:white;padding-bottom: 20px;"></i>
+                <p>smoralber@gmail.com</p>
+            </div>
+            <div class="column col-sm-4 center-absolute" style="text-align: center;">
+                <i class="fa fa-phone" style="font-size:48px;color:white;padding-bottom: 20px;"></i>
+                <p>656 987 824</p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
