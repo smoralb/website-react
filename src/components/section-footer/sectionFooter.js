e42a5b335301f1b7db78b1985cfbf47766490402
smoralber <smoralber@gmail.com>
2022-01-07 17:28:53 -0500

    Modified social media

--- src/components/section-footer/sectionFooter.js
+++ src/components/section-footer/sectionFooter.js
@@ -4,17 +4,14 @@ import './sectionFooter.css'
 const SectionFooter = () => (
   <footer className='footer'>
     <div className='social-container'>
-      <a href='https://twitter.com/smoralber' target='_blank'>
-        <img className='social-media' src='images/twitter-icon-white.jpg' />
+      <a href='https://twitter.com/smoralber'>
+        <img className='social-media' src='images/medium-logo.svg' />
       </a>
-      <a
-        href='https://www.linkedin.com/in/sergiomoralbermudez/'
-        target='_blank'
-      >
-        <img className='social-media' src='images/linkedin-white-icon.jpg' />
+      <a href='https://www.linkedin.com/in/sergiomoralbermudez/'>
+        <img className='social-media' src='images/linkedin-logo.svg' />
       </a>
-      <a href='https://github.com/smoralb' target='_blank'>
-        <img className='social-media' src='images/github-white-icon.jpg' />
+      <a href='https://github.com/smoralb'>
+        <img className='social-media' src='images/github-logo.svg' />
       </a>
     </div>
     <p className='footer-text'>Made with ❤️ from Madrid, Spain.</p>
